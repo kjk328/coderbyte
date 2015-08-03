7c630677784b535e95b8ba82472da13aeda9c34d
kjk328 <kjk328@gmail.com>
2015-08-03 15:41:41 +0900

    Create CheckNums.rb

---
+++ CheckNums.rb
@@ -0,0 +1,11 @@
+def CheckNums(num1,num2)
+	if num2 > num1
+      report = true
+      elsif num2 == num1
+      report = -1
+    else
+      report = false
+  # code goes here
+      return report
+    end
+end
